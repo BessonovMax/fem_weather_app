5cad73fcb50dbb10209fd31a1d302c4d1d78b947
Maks Bessonov <max8essonov@gmail.com>
2026-01-14 14:10:44 +0500

    Add new assets and style guide
    - Added new image assets: icon-storm.webp, icon-sunny.webp, icon-units.svg, and logo.svg.
    - Created a style guide (style-guide.md) detailing layout, colors, and typography for the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-
 function App() {
-
   return (
     <>
-      
+      Units Switch to Imperial/Metric Temperature Celsius (°C) Fahrenheit (°F)
+      Wind Speed km/h mph Precipitation Millimeters (mm) Inches (in) How's the
+      sky looking today? Search for a city, e.g., New York Search Feels like
+      Humidity Wind Precipitation Daily forecast Hourly forecast
+      <div className="text-center text-sm">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://github.com/BessonovMax" target="blank">
+          Max Bessonov
+        </a>
+        .
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
