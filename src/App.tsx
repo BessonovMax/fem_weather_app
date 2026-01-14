f8272fb551df669687aa99452621a6296e8ec5a2
Maks Bessonov <max8essonov@gmail.com>
2026-01-14 22:33:30 +0500

    Add DropDownMenu and Search components; refactor App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,25 @@
 import Logo from "./assets/images/logo.svg";
-import SearchIcon from "./assets/images/icon-search.svg";
-import IconUnits from "./assets/images/icon-units.svg";
-import DropDownIcon from "./assets/images/icon-dropdown.svg";
+import DropDownMenu from "./DropDownMenu";
+
+import Search from "./Search";
 
 function App() {
   return (
-    <div className="flex min-h-dvh flex-col gap-12 bg-neutral-900 p-4 text-white">
-      <header className="flex items-center justify-between">
-        <img className="h-[28px]" src={Logo} alt="Logo" />
-        <div className="text-preset-8 md:text-preset-7 flex cursor-pointer gap-1.5 rounded-md bg-neutral-800 px-2.5 py-2">
-          <img className="w-[14px] md:w-[16px]" src={IconUnits} alt="Units" />
-          Units
-          <img
-            className="w-[9px] md:w-[12px]"
-            src={DropDownIcon}
-            alt="Drop down menu"
-          />
+    <div className="min-h-dvh bg-neutral-900 p-4 md:p-6 lg:py-12">
+      <div className="mx-auto flex flex-col gap-12 text-white lg:max-w-[1216px] lg:gap-16">
+        <header className="flex items-center justify-between">
+          <img className="h-[28px] md:h-[40px]" src={Logo} alt="Logo" />
+          <DropDownMenu />
+        </header>
+        <div className="text-preset-2 self-center text-center md:max-w-[482px] lg:max-w-full">
+          How's the sky looking today?
         </div>
-      </header>
-      <div className="text-preset-2 text-center">
-        How's the sky looking today?
-      </div>
-      <form className="text-preset-5 flex flex-col gap-3">
-        <div className="relative flex items-center">
-          <img
-            className="pointer-events-none absolute pl-6"
-            src={SearchIcon}
-            alt="Search"
-          />
-          <input
-            className="w-full cursor-pointer rounded-xl bg-neutral-800 py-4 pr-4 pl-[3.75rem] text-neutral-200"
-            type="search"
-            placeholder="Search for a place..."
-          ></input>
-        </div>
-        <div className="cursor-pointer rounded-xl bg-blue-500 py-4 text-center">
-          Search
-        </div>
-      </form>
-      {/* Units Switch to Imperial/Metric Temperature Celsius (°C) Fahrenheit (°F)
+        <Search />
+        {/* Units Switch to Imperial/Metric Temperature Celsius (°C) Fahrenheit (°F)
       Wind Speed km/h mph Precipitation Millimeters (mm) Inches (in) How's the
       sky looking today? Search for a city, e.g., New York Search Feels like
       Humidity Wind Precipitation Daily forecast Hourly forecast */}
+      </div>
     </div>
   );
 }
---
+++ src/Search.tsx
@@ -0,0 +1,23 @@
+import SearchIcon from "./assets/images/icon-search.svg";
+
+export default function Search() {
+  return (
+    <form className="text-preset-5 flex flex-col gap-3 md:flex-row lg:w-[656px] lg:self-center">
+      <div className="relative flex grow-1 items-center">
+        <img
+          className="pointer-events-none absolute pl-6"
+          src={SearchIcon}
+          alt="Search"
+        />
+        <input
+          className="w-full cursor-pointer rounded-xl bg-neutral-800 py-4 pr-4 pl-[3.75rem] text-neutral-200"
+          type="search"
+          placeholder="Search for a place..."
+        ></input>
+      </div>
+      <div className="cursor-pointer rounded-xl bg-blue-500 py-4 text-center md:px-6">
+        Search
+      </div>
+    </form>
+  );
+}
